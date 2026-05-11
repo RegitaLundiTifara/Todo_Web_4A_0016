a4ccc07f3bc54e1dff50c16f070aa2097b3b11af
RegitaLundiTifara <regita.tifara24@student.uisi.ac.id>
2026-05-11 21:14:16 +0700

    Javascript dasar

---
+++ TodoWeb_4A_0016/script.js
@@ -0,0 +1,7 @@
+tTugas = document.getElementById("inputTugas");
+const btntambah = document.getElementById("btnTambah");
+const daftarTugas = document.getElementById("daftarTugas");
+const inputTanggal = document.getElementById("inputTanggal");
+const inputStatus = document.getElementById("inputStatus");
+
+let dataEdit = null;
